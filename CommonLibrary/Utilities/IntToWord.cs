acd91bea4b1107f4e0daafab7f176212ef1ea0d7
middas <trek2die4!>
2012-01-21 22:52:58 -0700

    cleaned up more code

--- CommonLibrary/Utilities/IntToWord.cs
+++ CommonLibrary/Utilities/IntToWord.cs
@@ -8,33 +8,57 @@ namespace CommonLibrary.Utilities
         public static string ConvertIntToWords(int i)
         {
             int abs = System.Math.Abs(i);
+
             if (i == 0)
+            {
                 return "zero";
+            }
+
             int count = 1;
+
             while ((abs /= 1000) > 0)
+            {
                 count++;
+            }
+
             abs = System.Math.Abs(i);
+
             int[] groups = new int[count];
+
             for (int x = 0; x < groups.Length; x++)
             {
                 groups[x] = abs % 1000;
                 abs /= 1000;
             }
+
             StringBuilder sb = new StringBuilder();
+
             for (int x = 0; x < groups.Length - 1; x++)
+            {
                 sb.Insert(0, string.Format(" {0}", ConvertToWords(groups[x], x + 1)));
+            }
+
             sb.Insert(0, string.Format("{0}", ConvertToWords(groups[count - 1], count)));
+
             if (i < 0)
+            {
                 sb.Insert(0, "negative ");
+            }
+
             string value = sb.ToString();
+
             while (value.Contains("  "))
+            {
                 value = value.Replace("  ", " ");
+            }
+
             return value.Trim();
         }
 
         private static string GetNumberPosition(int i)
         {
             string s = "";
+
             switch (i)
             {
                 case 0:
@@ -50,24 +74,36 @@ namespace CommonLibrary.Utilities
                     s = " billion";
                     break;
             }
+
             return s;
         }
 
         private static string ConvertToWords(int threeDigits, int position)
         {
             string s = "";
+
             if (threeDigits > 999)
+            {
                 throw new Exception("Cannot convert more than 3 numbers at a time");
+            }
+
             int hundreds = threeDigits / 100;
             int tens = (threeDigits % 100);
             int units = (tens % 10);
+
             tens /= 10;
+
             if (hundreds == 0 && tens == 0)
+            {
                 s = GetWordFromInt(units);
+            }
             else
             {
                 if (hundreds > 0)
+                {
                     s = string.Format("{0} hundred", GetWordFromInt(hundreds));
+                }
+
                 if (tens > 0)
                 {
                     if (tens == 1)
@@ -138,23 +174,36 @@ namespace CommonLibrary.Utilities
                                 s = string.Format("{0} {1}", s, "ninety");
                                 break;
                         }
+
                         if (units > 0)
+                        {
                             s = string.Format("{0}-", s);
+                        }
                     }
                 }
                 else if (units > 0)
+                {
                     s = string.Format("{0} and", s);
+                }
+
                 if (units > 0 && tens != 1)
+                {
                     s = string.Format("{0}{1}", s, GetWordFromInt(units));
+                }
             }
+            
             if (hundreds > 0 || tens > 0 || units > 0)
+            {
                 s = string.Format("{0}{1}", s, GetNumberPosition(position));
+            }
+
             return s;
         }
 
         private static string GetWordFromInt(int i)
         {
             string s = "";
+
             switch (i)
             {
                 case 1:
@@ -185,6 +234,7 @@ namespace CommonLibrary.Utilities
                     s = "nine";
                     break;
             }
+
             return s;
         }
     }
