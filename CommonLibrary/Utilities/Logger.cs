f68ca348248d1b0253dc1d1860cca9a352fe2229
middas <trek2die4!>
2012-01-21 23:30:23 -0700

    added Logger class
    version tick

---
+++ CommonLibrary/Utilities/Logger.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CommonLibrary.Patterns;
+
+namespace CommonLibrary.Utilities
+{
+    public class Logger
+    {
+        public bool IsInitialized
+        {
+            get;
+            private set;
+        }
+
+        private Action<string> LogAction;
+
+        public static Logger GetLogger
+        {
+            get
+            {
+                return Singleton<Logger>.GetInstance();
+            }
+        }
+
+        public void Initialize(Action<string> logAction)
+        {
+            if (!IsInitialized)
+            {
+                LogAction = logAction;
+            }
+        }
+
+        public void Log(string data)
+        {
+            LogAction(data);
+        }
+    }
+}
