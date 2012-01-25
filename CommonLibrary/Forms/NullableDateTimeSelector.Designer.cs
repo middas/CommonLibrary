07e3496b847f34b6a5b86f634f8532b4ccc49a3c
middas <trek2die4!>
2012-01-25 00:33:22 -0700

    removed observer
    added nullabledatetimeselector

---
+++ CommonLibrary/Forms/NullableDateTimeSelector.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace CommonLibrary.Forms
+{
+    partial class NullableDateTimeSelector
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DateTimePickerMain = new System.Windows.Forms.DateTimePicker();
+            this.TextBoxMain = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // DateTimePickerMain
+            // 
+            this.DateTimePickerMain.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.DateTimePickerMain.Location = new System.Drawing.Point(0, 0);
+            this.DateTimePickerMain.Name = "DateTimePickerMain";
+            this.DateTimePickerMain.Size = new System.Drawing.Size(200, 20);
+            this.DateTimePickerMain.TabIndex = 0;
+            // 
+            // TextBoxMain
+            // 
+            this.TextBoxMain.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.TextBoxMain.Location = new System.Drawing.Point(0, 0);
+            this.TextBoxMain.Name = "TextBoxMain";
+            this.TextBoxMain.Size = new System.Drawing.Size(167, 20);
+            this.TextBoxMain.TabIndex = 1;
+            this.TextBoxMain.Leave += new System.EventHandler(this.TextBoxMain_Leave);
+            // 
+            // NullableDateTimeSelector
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.TextBoxMain);
+            this.Controls.Add(this.DateTimePickerMain);
+            this.Name = "NullableDateTimeSelector";
+            this.Size = new System.Drawing.Size(200, 20);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker DateTimePickerMain;
+        private System.Windows.Forms.TextBox TextBoxMain;
+    }
+}
---
+++ CommonLibrary/Forms/NullableDateTimeSelector.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using CommonLibrary.Native;
+using System.Data.SqlTypes;
+
+namespace CommonLibrary.Forms
+{
+    public partial class NullableDateTimeSelector : UserControl
+    {
+        public DateTime? Date
+        {
+            get
+            {
+                DateTime? dateTime;
+
+                if (TextBoxMain.Text.IsNullOrEmptyTrim())
+                {
+                    dateTime = null;
+                }
+                else
+                {
+                    dateTime = DateTimePickerMain.Value;
+                }
+
+                return dateTime;
+            }
+            set
+            {
+                DateTimePickerMain.Value = value.HasValue ? value.Value : SqlDateTime.MinValue.Value;
+
+                if (DateTimePickerMain.Value == SqlDateTime.MinValue.Value)
+                {
+                    TextBoxMain.Text = string.Empty;
+                }
+                else
+                {
+                    TextBoxMain.Text = DateTimePickerMain.Text;
+                }
+            }
+        }
+
+        public NullableDateTimeSelector()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBoxMain_Leave(object sender, EventArgs e)
+        {
+            if (TextBoxMain.Text.IsNullOrEmptyTrim())
+            {
+                Date = null;
+            }
+            else
+            {
+                DateTime date;
+
+                if (DateTime.TryParse(TextBoxMain.Text, out date))
+                {
+                    Date = date;
+                }
+                else
+                {
+                    Date = null;
+                }
+            }
+        }
+    }
+}
